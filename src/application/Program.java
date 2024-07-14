d1371fe6a2d958371985df7a24a817ed90a6f30b
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-14 15:23:01 -0300

    exercicio resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,27 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int n, numPar = 0;
+		double mediaNumPar = 0.00;
+		
+		System.out.print("Quantos elementos vai ter o vetor? ");
+		n = sc.nextInt();
+		
+		int[] vetor = new int[n];
+		for(int i = 0; i < n; i++) {
+			System.out.print("Digite um numero: ");
+			vetor[i] = sc.nextInt();
+			if(vetor[i] % 2 == 0) {
+				mediaNumPar += vetor[i];
+				numPar++;
+			}
+		}
+		if(numPar > 0) {
+			System.out.printf("MEDIA DOS PARES = %.1f", mediaNumPar / numPar);
+		} else {
+			System.out.println("NENHUM NUMERO PAR");
+		}
+		
 		
 		sc.close();
 	}
