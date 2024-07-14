08bdf64406b42b86d4b2336839eb2968b0d49d05
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-14 15:13:21 -0300

    primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// exericio DevSuperior - media_pares
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+
+}
